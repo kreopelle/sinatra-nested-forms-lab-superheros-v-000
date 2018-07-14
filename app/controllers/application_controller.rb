ce47683c0afb239de8b1b91657ef3cd64a1349bc
Kayla Reopelle <kreopelle@gmail.com>
2018-07-14 19:20:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
       team = Team.new(params[:team][:name], params[:team][:motto])
 
       params[:team][:members].each do |details|
-        Member.new(details)
+        Member.new()
       end
 
       members = Member.all
